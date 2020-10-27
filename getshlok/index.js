3f678e9051b52eca3d7088151c0b106841d1e8f6
maswadkar <maswadkar@gmail.com>
2020-10-27 13:55:21 +0100

    completed dashak pahila

--- getshlok/index.js
+++ getshlok/index.js
@@ -1,13 +1,8 @@
+var fs = require('fs');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    data = JSON.parse(fs.readFileSync('data/dasbodh.json', 'utf8'))
+    rand_shlok = Math.floor(Math.random() * data.length)
+    console.log(data.length)
+    context.res = { 'body': data[rand_shlok]['shlok'] }
 }
