866c0a57d090631ce590b54f1432825404e2d264
maswadkar <maswadkar@gmail.com>
2021-04-29 19:29:35 +0200

    added paragraph comments if they are empty

--- getshlokbydashak/index.js
+++ getshlokbydashak/index.js
@@ -3,21 +3,21 @@ var fs = require('fs');
 
 module.exports = async function (context, req) {
 
-    var bychapter = "Please provide Chapter Number"
-    if(req.query.chapter){
-    console.log(req.query.chapter)
-    data = JSON.parse(fs.readFileSync('data/all.json', 'utf8'))
+    var bychapter = "Please provide chapter Number AND paragraph number"
+    if (req.query.chapter && req.query.paragraph) {
+        console.log(req.query.chapter)
+        data = JSON.parse(fs.readFileSync('data/all.json', 'utf8'))
 
-    function contains(value) {
-        return (value.chapter == req.query.chapter) && (value.paragraph == req.query.paragraph);
+        function contains(value) {
+            return (value.chapter == req.query.chapter) && (value.paragraph == req.query.paragraph);
+        }
+
+        bychapter = data.filter(contains)
+
+    } else {
+        console.log("Blank")
     }
 
-    bychapter = data.filter(contains)
-
-}else{
-    console.log("Blank")
-}
-    
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: bychapter
