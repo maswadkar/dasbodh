1136d2972cb0c9329d6f72f4b804bb85f455d0b6
maswadkar <maswadkar@gmail.com>
2021-04-29 19:19:24 +0200

    changed reference to all.json from dasbodh.json

--- getshlokbydashak/index.js
+++ getshlokbydashak/index.js
@@ -6,7 +6,7 @@ module.exports = async function (context, req) {
     var bychapter = "Please provide Chapter Number"
     if(req.query.chapter){
     console.log(req.query.chapter)
-    data = JSON.parse(fs.readFileSync('data/dasbodh.json', 'utf8'))
+    data = JSON.parse(fs.readFileSync('data/all.json', 'utf8'))
 
     function contains(value) {
         return (value.chapter == req.query.chapter) && (value.paragraph == req.query.paragraph);
