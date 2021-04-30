f4130e0d7ccddff4252ece9955b69eb7dd1a951a
maswadkar <maswadkar@gmail.com>
2021-04-30 22:37:55 +0200

    changed source to all.json

--- getshlok/index.js
+++ getshlok/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    data = JSON.parse(fs.readFileSync('data/dasbodh.json', 'utf8'))
+    data = JSON.parse(fs.readFileSync('data/all.json', 'utf8'))
     rand_shlok = Math.floor(Math.random() * data.length)
     console.log('the shlok# ' + rand_shlok)
     context.res = { 'body': data[rand_shlok]}
